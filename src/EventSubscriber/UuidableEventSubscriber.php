8b5a46b51cccb6463a3a64befac1ae46fdd10724
Gossler Karoly <connor@connor.hu>
2025-04-01 18:52:16 +0200

    changed deprecated orm3 methods and service subscribing

--- src/EventSubscriber/UuidableEventSubscriber.php
+++ src/EventSubscriber/UuidableEventSubscriber.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Knp\DoctrineBehaviors\EventSubscriber;
 
-use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
-use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Event\PrePersistEventArgs;
 use Doctrine\ORM\Events;
 use Knp\DoctrineBehaviors\Contract\Entity\UuidableInterface;
 
-final class UuidableEventSubscriber implements EventSubscriberInterface
+#[AsDoctrineListener(event: Events::prePersist)]
+final class UuidableEventSubscriber
 {
     public function loadClassMetadata(LoadClassMetadataEventArgs $loadClassMetadataEventArgs): void
     {
@@ -35,21 +36,13 @@ final class UuidableEventSubscriber implements EventSubscriberInterface
         ]);
     }
 
-    public function prePersist(LifecycleEventArgs $lifecycleEventArgs): void
+    public function prePersist(PrePersistEventArgs $eventArgs): void
     {
-        $entity = $lifecycleEventArgs->getEntity();
+        $entity = $eventArgs->getObject();
         if (! $entity instanceof UuidableInterface) {
             return;
         }
 
         $entity->generateUuid();
-    }
-
-    /**
-     * @return string[]
-     */
-    public function getSubscribedEvents(): array
-    {
-        return [Events::loadClassMetadata, Events::prePersist];
     }
 }
