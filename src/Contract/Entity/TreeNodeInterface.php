5bffeeb6069b9fcf125f4d1e75d3d780cc7d93a7
Gossler Karoly <connor@connor.hu>
2025-04-02 08:13:20 +0200

    added key type

--- src/Contract/Entity/TreeNodeInterface.php
+++ src/Contract/Entity/TreeNodeInterface.php
@@ -57,7 +57,7 @@ interface TreeNodeInterface
     public function addChildNode(self $treeNode): void;
 
     /**
-     * @return Collection<TreeNodeInterface>
+     * @return Collection<int, TreeNodeInterface>
      */
     public function getChildNodes(): Collection;
 
