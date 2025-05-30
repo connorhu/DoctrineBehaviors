ba2542c26b324adbeacf4f5f7e57be05a730e88f
Gossler Karoly <connor@connor.hu>
2025-05-30 10:41:06 +0200

    error handler cleanup

--- tests/AbstractBehaviorTestCase.php
+++ tests/AbstractBehaviorTestCase.php
@@ -75,4 +75,26 @@ abstract class AbstractBehaviorTestCase extends TestCase
     {
         return $this->container->get($type);
     }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->restoreExceptionHandler();
+    }
+
+    protected function restoreExceptionHandler(): void
+    {
+        while (true) {
+            $previousHandler = set_exception_handler(static fn() => null);
+
+            restore_exception_handler();
+
+            if ($previousHandler === null) {
+                break;
+            }
+
+            restore_exception_handler();
+        }
+    }
 }
